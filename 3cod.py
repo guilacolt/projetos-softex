2a311a5f235f758463836b9f6cf5e742b69f4206
guilacolt <79493286+guilacolt@users.noreply.github.com>
2022-08-12 01:26:26 -0300

    Add files via upload

---
+++ 3cod.py
@@ -0,0 +1,17 @@
+
+x = "Olá, eu sou o goku!"
+x = x.replace("o goku", "seu pai")
+print(x)
+
+x = "Eu não sou o ratinho"
+x = x.upper()
+print(x)
+
+x = "Eu não fui comprar cigarro"
+len(x)
+print(len(x))
+
+
+
+
+
