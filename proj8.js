febf91f1da8290e8b327fa5284ebc2f16f0ce1ff
guilacolt <79493286+guilacolt@users.noreply.github.com>
2022-09-23 03:31:30 -0300

    Add files via upload

---
+++ proj8.js
@@ -0,0 +1,15 @@
+let corrida = {
+    lugar: 'mônaco', 
+    tempo: 'chuvoso', 
+    ganhador: 'Ayrton Senna',
+};
+for(var stats in corrida){
+    console.log(stats)
+};
+
+let pilotos = ['Ayrton Senna ', 'Alain Prost', 'Niki Lauda']
+
+for(var nome of pilotos){
+    console.log(nome)
+};
+
