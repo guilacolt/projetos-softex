1c61b2d0d3c523cf4a7ee71d51f1aee5ae3a9aed
Guilherme Martins <79493286+guilhermeVNmartins@users.noreply.github.com>
2022-12-08 12:58:11 -0300

    Update index.js

--- m.3.p.6/index.js
+++ m.3.p.6/index.js
@@ -1,6 +1,5 @@
 const TextEditor = require('./editor');
 
-// Criando os observadores observadores
 function Observer1 (data){ 
     let observer = data 
     console.log (observer);
@@ -18,15 +17,12 @@ function Observer3 (data){
 
 let calcular = new TextEditor ();
 
-// Colocando e removendo as linhas
 calcular.insertLine();
 calcular.removeLine();
 
-//inscrevendo os observadores
 calcular.subscribe(Observer1);
 calcular.subscribe(Observer2);
 calcular.subscribe(Observer3);
 
-//desiscrevendo os observadores 
 calcular.unsubscribe(Observer2);
 calcular.unsubscribe(Observer3);
