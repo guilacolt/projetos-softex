a9bb4aa774a71c058cdb44e99de37b1f4e678527
Guilherme Martins <79493286+guilhermeVNmartins@users.noreply.github.com>
2023-03-24 16:27:54 -0300

    Update m6.p6.js

--- m6.p6.js
+++ m6.p6.js
@@ -2,20 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// Configuração do middleware para o Express
+
 app.use(express.json())
 
-// Dados de exemplo
+
 const alunos = [  { id: 1, nome: 'Alice', idade: 20 },  { id: 2, nome: 'Bob', idade: 21 },  { id: 3, nome: 'Charlie', idade: 22 },]
 
-// Rotas para as operações CRUD
 
-// Listar todos os alunos
 app.get('/alunos', (req, res) => {
   res.status(200).json(alunos)
 })
 
-// Buscar um aluno pelo ID
 app.get('/alunos/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const aluno = alunos.find(a => a.id === id)
@@ -26,7 +23,7 @@ app.get('/alunos/:id', (req, res) => {
   }
 })
 
-// Criar um novo aluno
+
 app.post('/alunos', (req, res) => {
   const aluno = req.body
   aluno.id = alunos.length + 1
@@ -34,7 +31,7 @@ app.post('/alunos', (req, res) => {
   res.status(201).json(aluno)
 })
 
-// Atualizar um aluno pelo ID
+
 app.put('/alunos/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const alunoIndex = alunos.findIndex(a => a.id === id)
@@ -48,7 +45,7 @@ app.put('/alunos/:id', (req, res) => {
   }
 })
 
-// Deletar um aluno pelo ID
+
 app.delete('/alunos/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const alunoIndex = alunos.findIndex(a => a.id === id)
